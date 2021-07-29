996f69411f720eae5a76f2a72a8ff505d77ca97e
Hugo Robitaillie <hugo@la-ligne-web.com>
2021-07-29 17:51:07 +0200

    solution https://github.com/OpenClassrooms-Student-Center/GameOn-website-FR/issues/1

---
+++ js/modal.js
@@ -0,0 +1,34 @@
+function editNav() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+const closeBtn = document.querySelector(".close");
+console.log(closeBtn);
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// Close modal event
+closeBtn.addEventListener("click", closeModal);
+
+
+// Close modal form
+function closeModal(){
+  modalbg.style.display = "none";
+  console.log("close click")
+}
