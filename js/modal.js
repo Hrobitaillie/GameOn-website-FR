efa0003a1d807acdad05803555152338c0968d06
Hugo Robitaillie <hugo@la-ligne-web.com>
2021-08-09 14:54:32 +0200

    Issue 2 & 3

--- js/modal.js
+++ js/modal.js
@@ -11,9 +11,7 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
-
-const closeBtn = document.querySelector(".close");
-console.log(closeBtn);
+const submit = document.querySelector(".btn-submit");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -23,12 +21,145 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+
+//DOM Element Close button
+const closeBtn = document.querySelector(".close");
+
 // Close modal event
 closeBtn.addEventListener("click", closeModal);
-
 
 // Close modal form
 function closeModal(){
   modalbg.style.display = "none";
-  console.log("close click")
+  console.log("close click");
+}
+
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+
+//DOM Elements validation
+const form = document.getElementById('form');
+const first = document.getElementById('first');
+const last = document.getElementById('last');
+const email = document.getElementById('email');
+const birth = document.getElementById('birthdate');
+const quantity = document.getElementById('quantity');
+const loc = document.getElementById('location');
+const condition = document.getElementById('checkbox1')
+
+//DOM Elements Erorr
+const firstError = document.getElementById('firstError');
+const lastError = document.getElementById('lastError');
+const emailError = document.getElementById('emailError');
+const birthError = document.getElementById('birthError');
+const quantityError = document.getElementById('quantityError');
+const locError = document.getElementById('locError');
+const conditionError = document.getElementById('conditionError');
+
+
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+//---------------------------------------------------------------
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+})
+
+function validate(){
+
+  let firstOk;
+  let lastOk;
+  let mailOk;
+  let birthOk;
+  let quantityOk;
+  let locOk;
+  let conditionOk;
+
+  // Testing lirstname content
+  if(first.value<2 || first.value == Number){
+    firstError.style.display="block";
+    firstError.innerHTML = "Ecrivez un prénom de plus de 2 lettres";
+    firstError.style.color="red";
+    firstError.style.fontSize="14px";
+  }else{
+    firstError.style.display="none";
+    firstOk=true;
+  }
+
+  // Testing lastname content
+  if(last.value<2 || last.value == Number){
+    lastError.style.display="block";
+    lastError.innerHTML = "Ecrivez un prénom de plus de 2 lettres";
+    lastError.style.color="red";
+    lastError.style.fontSize="14px";
+  }else{
+    lastError.style.display="none";
+    lastOk=true;
+  }
+
+  //checking mail format
+  const mailformat = /^[\w-]+(\.[\w-]+)*@([a-z0-9-]+(\.[a-z0-9-]+)*?\.[a-z]{2,6}|(\d{1,3}\.){3}\d{1,3})(:\d{4})?$/;
+  if(!(mailformat.test(email.value))){
+    emailError.style.display="block";
+    emailError.innerHTML = "Merci d'entrer un Email valide";
+    emailError.style.color="red";
+    emailError.style.fontSize="14px";
+  }else{
+    emailError.style.display="none";
+    mailOk=true;
+  }
+  //checking quantity
+  if((quantity.value=="" && !(quantity.value == Number))){
+    quantityError.style.display="block";
+    quantityError.innerHTML = "Merci d'entrer un nombre de tournois";
+    quantityError.style.color="red";
+    quantityError.style.fontSize="14px";
+  }else{
+    quantityError.style.display="none";
+    quantityOk=true;
+  }
+
+  // On test les boutons radio et sauvegarde le bouton coché
+  const formulaire = document.forms['reserve']
+  const radios = formulaire.elements['location'];
+  var value="";
+  for (var i = 0; i < radios.length; i++){
+    if((radios[i].type === 'radio' && radios[i].checked)) {
+      value = radios[i].value;
+      console.log(value);
+    }
+  }
+  //vérification si un bouton radio a été coché
+  if(value == ""){
+    locError.style.display="block";
+    locError.innerHTML = "Merci de cocher une ville";
+    locError.style.color="red";
+    locError.style.fontSize="14px";
+  }else{
+    locError.style.display="none";
+    locOk=true;
+  }
+  if(!condition.checked){
+    conditionError.style.display="block";
+    conditionError.innerHTML = "Vous devez accepter les conditions d'utilisations";
+    conditionError.style.color="red";
+    conditionError.style.fontSize="14px";
+  }else{
+    conditionError.style.display="none";
+    conditionOk=true;
+  }
+  console.log("firstOk =" + firstOk);
+  console.log("lastOk =" + lastOk);
+  console.log("mailOk =" + mailOk);
+  console.log("quantityOk =" + quantityOk);
+  console.log("locOk =" + locOk);
+  console.log("conditionOk =" + conditionOk);
+
+  //Checking if everything is ok
+  if(firstOk == true && lastOk == true && mailOk == true && birthOk == true && quantityOk == true && locOk == true && conditionOk == true){
+    modalbg.style.display = "none";
+  }
 }
